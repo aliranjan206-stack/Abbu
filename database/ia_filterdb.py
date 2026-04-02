c938f7fe9ec1481bd752ef39270f6f4c9a4ef98b
kaluanubhav52 <kaluanubhav52@gmail.com>
2026-04-02 09:42:31 +0530

    Change import from pyrogram to kurigram

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -2,7 +2,7 @@ import logging
 from struct import pack
 import re
 import base64
-from pyrogram.file_id import FileId
+from kurigram.file_id import FileId
 from typing import Dict, List
 from collections import defaultdict
 from pymongo.errors import DuplicateKeyError
